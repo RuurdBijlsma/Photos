92ee438b5ac25780baa3b986ebaf93f62dc7adc7
Ruurd <ruurd@bijlsma.dev>
2021-03-14 20:31:40 +0100

    Add default emotes

---
+++ src/modules/twimote-bot/getEmotes/defaultEmotes.js
@@ -0,0 +1,14 @@
+export default [
+    {name: "LUL", url: "https://cdn.frankerfacez.com/emoticon/143984/4"},
+    {name: ":)", url: "https://static-cdn.jtvnw.net/emoticons/v1/1/5.0"},
+    {name: "Kappa", url: "https://cdn.frankerfacez.com/emoticon/460894/4"},
+    {name: "BibleThump", url: "https://cdn.frankerfacez.com/emoticon/269642/4"},
+    {name: "4Head", url: "https://static-cdn.jtvnw.net/emoticons/v1/354/1.0"},
+    {name: "KreyGasm", url: "https://cdn.frankerfacez.com/emoticon/419523/4"},
+    {name: "ResidentSleeper", url: "https://cdn.frankerfacez.com/emoticon/428014/4"},
+    {name: "WutFace", url: "https://cdn.frankerfacez.com/emoticon/434628/1"},
+    {name: "HeyGuys", url: "https://cdn.frankerfacez.com/avatar/twitch/61772429"},
+    {name: "KappaPride", url: "https://cdn.frankerfacez.com/emoticon/434617/1"},
+    {name: "NotLikeThis", url: "https://cdn.frankerfacez.com/emoticon/481082/4"},
+    {name: "PogChamp", url: "https://static-cdn.jtvnw.net/emoticons/v1/305954156/5.0"},
+]
--- src/modules/twimote-bot/getEmotes/processEmotes.js
+++ src/modules/twimote-bot/getEmotes/processEmotes.js
@@ -2,13 +2,14 @@ import rawEmotes from "./rawEmotes.js";
 import probe from "probe-image-size";
 import fs from "fs";
 import path from "path";
+import defaultEmotes from "./defaultEmotes.js";
 
-
+let emotes = rawEmotes.concat(defaultEmotes);
 let result = {}, i = 0;
-for (let {name, url} of rawEmotes) {
+for (let {name, url} of emotes) {
     i++;
     if (result.hasOwnProperty(name)) {
-        console.log(`[${i}/${rawEmotes.length}] Skipping ${name}, already included`);
+        console.log(`[${i}/${emotes.length}] Skipping ${name}, already included`);
         continue;
     }
     let data = await probe(url);
@@ -17,7 +18,7 @@ for (let {name, url} of rawEmotes) {
         animated: data.type.toLowerCase().includes('gif'),
         url: url,
     }
-    console.log(`[${i}/${rawEmotes.length}] Processed ${name}`);
+    console.log(`[${i}/${emotes.length}] Processed ${name}`);
 }
 
 let emotesFile = path.resolve('src/modules/twimote-bot/emotes.js');
