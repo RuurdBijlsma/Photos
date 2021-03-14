3257d48a059b5887055ecadd540c442aa017933b
Ruurd <ruurd@bijlsma.dev>
2021-03-14 17:43:51 +0100

    Remove top level await

--- src/modules/twimote-bot/test-twimote.js
+++ src/modules/twimote-bot/test-twimote.js
@@ -1,4 +1,8 @@
 import {getFileType, text2media} from "./twimote.js";
 
-await text2media("R I OMEGALUL L U");
-await text2media("PepePls hello peepoClap")
+async function test() {
+    await text2media("R I OMEGALUL L U");
+    await text2media("PepePls hello peepoClap")
+}
+
+test().then(() => console.log('test complete'));
