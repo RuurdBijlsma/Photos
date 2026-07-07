b185d03dbfde639fe3b4730199fe1d71cdfc67b5
Ruurd Bijlsma <ruurd@bijlsma.dev>
2026-07-07 22:23:47 +0200

    Change icons

--- web/src/vues/layouts/OnboardingLayout.vue
+++ web/src/vues/layouts/OnboardingLayout.vue
@@ -41,7 +41,7 @@ withDefaults(
 }
 
 .big-image {
-  background-image: url('@/assets/img/app-no-bg-1024.png');
+  background-image: url('@/assets/img/logo/transparent/512.png');
   width: 100px;
   height: 100px;
   background-size: 90%;
