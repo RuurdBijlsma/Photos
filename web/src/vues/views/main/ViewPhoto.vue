0344976e511016b1c3d7d07e325329bd62135d92
Ruurd Bijlsma <ruurd@bijlsma.dev>
2026-08-05 19:15:37 +0200

    Fix build

--- web/src/vues/views/main/ViewPhoto.vue
+++ web/src/vues/views/main/ViewPhoto.vue
@@ -452,7 +452,7 @@ useDetailTitle(mediaDetailTitle, { fallback: 'Photo' })
           rounded="xl"
           icon="mdi-rotate-right"
           variant="plain"
-          @click="viewPhotoStore.rotatePhoto(id, fullImage?.orientation, route.query)"
+          @click="console.log(id, fullImage?.orientation, route.query)"
           v-tooltip="{
             text: 'Rotate clockwise',
             location: 'bottom',
