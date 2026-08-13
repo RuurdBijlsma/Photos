eb404757b99244c5d40a8e7ade551c99773fa2d8
Ruurd Bijlsma <ruurd@bijlsma.dev>
2026-08-13 19:13:53 +0200

    Cache cluster photos asset for integration test, fix hf cache folder

--- server/crates/libs/app_state/src/constants.rs
+++ server/crates/libs/app_state/src/constants.rs
@@ -14,7 +14,7 @@ pub const CACHE_FOLDER: &str = "cache";
 pub const ON_DEMAND_THUMBNAIL_CACHE_FOLDER: &str = "cache/on-demand-thumbnails";
 pub const TUS_UPLOADS_FOLDER: &str = "tus/uploads";
 pub const TUS_LOCKS_FOLDER: &str = "tus/locks";
-pub const HUGGINGFACE_CACHE_FOLDER: &str = "cache/hugingface";
+pub const HUGGINGFACE_CACHE_FOLDER: &str = "cache/huggingface";
 // hosted folders (in app_data)
 pub const HOSTED_FOLDER: &str = "hosted";
 pub const PANO_FOLDER: &str = "hosted/pano";
