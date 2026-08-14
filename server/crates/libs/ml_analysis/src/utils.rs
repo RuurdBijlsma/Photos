799a90d5c2c7e45d5b784d35bc0647c18c1e8870
Ruurd Bijlsma <ruurd@bijlsma.dev>
2026-08-14 21:47:40 +0200

    Fix fast_image_analysis being async, which caused issues

--- server/crates/libs/ml_analysis/src/utils.rs
+++ server/crates/libs/ml_analysis/src/utils.rs
@@ -1,41 +1,38 @@
 use image::imageops::FilterType;
 use std::io;
 use std::path::Path;
-use tokio::process::Command;
 use tracing::error;
 
-pub async fn convert_media_file(
+pub fn convert_media_file_sync(
     input_path: &Path,
     output_path: &Path,
     image_out_size: u64,
 ) -> io::Result<()> {
-    let input = input_path.to_owned();
-    let output = output_path.to_owned();
-    let image_result = tokio::task::spawn_blocking(move || {
-        let img = image::open(&input).map_err(|e| io::Error::other(e.to_string()))?;
+    // Try native decoding and resizing
+    let image_result = (|| -> io::Result<()> {
+        let img = image::open(input_path).map_err(|e| io::Error::other(e.to_string()))?;
         let resized = img.resize(u32::MAX, image_out_size as u32, FilterType::Lanczos3);
 
         resized
-            .save(&output)
+            .save(output_path)
             .map_err(|e| io::Error::other(e.to_string()))?;
-        Ok::<(), io::Error>(())
-    })
-    .await;
-    if matches!(image_result, Ok(Ok(()))) {
+        Ok(())
+    })();
+
+    if image_result.is_ok() {
         return Ok(());
     }
 
-    // Fallback to ffmpeg
+    // Fallback to FFmpeg process
     let scale_filter = format!("scale=-2:{image_out_size}");
-    let ffmpeg_output = Command::new("ffmpeg")
+    let ffmpeg_output = std::process::Command::new("ffmpeg")
         .arg("-i")
         .arg(input_path)
         .arg("-vf")
         .arg(scale_filter)
         .arg("-y") // Overwrite output files without asking
         .arg(output_path)
-        .output()
-        .await?;
+        .output()?;
 
     if ffmpeg_output.status.success() {
         Ok(())
