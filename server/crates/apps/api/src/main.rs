b95523ac91f8ae56309caf0e253b4f26c56d491f
Ruurd Bijlsma <ruurd@bijlsma.dev>
2026-08-22 15:27:49 +0200

    Add photos_app so all apps can run in a single binary

--- server/crates/apps/api/src/main.rs
+++ server/crates/apps/api/src/main.rs
@@ -2,6 +2,9 @@ use api::serve;
 use clap::Parser;
 use color_eyre::Result;
 use common_services::binary_setup::setup_binary;
+use common_services::graceful_exit::get_kill_signal;
+use model_provider::ModelProvider;
+use std::sync::Arc;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,7 +16,20 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<()> {
     let (settings, pool) = setup_binary().await?;
-    serve(pool, settings, !Args::parse().no_scheduler).await?;
+    let shutdown_rx = get_kill_signal();
+    let model_provider = Arc::new(ModelProvider::new(
+        settings.clone(),
+        settings.scaler.unload_models_timeout,
+    ));
+
+    serve(
+        pool,
+        settings,
+        model_provider,
+        !Args::parse().no_scheduler,
+        shutdown_rx,
+    )
+        .await?;
 
     Ok(())
 }
